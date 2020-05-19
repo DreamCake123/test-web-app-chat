a5df1b80ee2ba9e72fa87943a524e3eccb73deb6
DreamCake123 <55613566+DreamCake123@users.noreply.github.com>
2020-05-19 18:31:03 +0800

    port 3000 -> process.env.PORT

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const socket = require('socket.io');
 //Initialization
 
 const app = express();
-const server = app.listen(3000, () => console.log("Server is on port 3000"));
+const server = app.listen(process.env.PORT, () => console.log("Server is on port 3000"));
 var io = socket(server);
 
 //Express 
