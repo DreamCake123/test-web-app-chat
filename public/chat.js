bd475bb2c6acabc0a8d49e366b19dae3063683ff
DreamCake123 <55613566+DreamCake123@users.noreply.github.com>
2020-05-19 18:43:48 +0800

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //Make connection with SOCKET IO
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost');
 //Query DOM
 var msg = document.getElementById('message-box'),
     send = document.getElementById('send'),
